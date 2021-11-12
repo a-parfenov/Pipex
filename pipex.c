0e150b981a94315c6c392aded738b0bd51dc493e
Parfenov Anton <v5pishka@yandex.ru>
2021-11-12 14:11:03 +0300

    Corrected the name of the functions

--- pipex.c
+++ pipex.c
@@ -6,13 +6,13 @@
 /*   By: aleslie <aleslie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 14:30:05 by aleslie           #+#    #+#             */
-/*   Updated: 2021/11/12 12:23:58 by aleslie          ###   ########.fr       */
+/*   Updated: 2021/11/12 13:54:39 by aleslie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*ft_collecting_path_cmd(char **path, char *command)
+char	*collecting_path_cmd(char **path, char *command)
 {
 	int		i;
 	int		valid;
@@ -35,7 +35,7 @@ char	*ft_collecting_path_cmd(char **path, char *command)
 	return (NULL);
 }
 
-char	**ft_pars_var_environ(char **env)
+char	**pars_var_environ(char **env)
 {
 	int		i;
 	char	**path;
@@ -58,7 +58,7 @@ char	**ft_pars_var_environ(char **env)
 ** 74 - Ввод с файла, 75 - Вывод в начало трубы
 ** 76 - Закрываем конец трубы, 77 - Парсим переменное окружение
 */
-void	ft_child_process(int *pipe_fd, char **argv, char **env)
+void	child_process(int *pipe_fd, char **argv, char **env)
 {
 	int		file1;
 	char	**path;
@@ -74,11 +74,11 @@ void	ft_child_process(int *pipe_fd, char **argv, char **env)
 	dup2(file1, 0);
 	dup2(pipe_fd[1], 1);
 	close(pipe_fd[0]);
-	path = ft_pars_var_environ(env);
+	path = pars_var_environ(env);
 	command = ft_split(argv[2], ' ');
-	all_file_path = ft_collecting_path_cmd(path, command[0]);
+	all_file_path = collecting_path_cmd(path, command[0]);
 	if (!all_file_path)
-		perror("Command not found {ft_child_process}");
+		perror("Command not found {child_process}");
 	execve(all_file_path, command, env);
 }
 
@@ -86,7 +86,7 @@ void	ft_child_process(int *pipe_fd, char **argv, char **env)
 ** 102 - Ввод с конца трубы, 103 - Вывод в файл
 ** 104 - Закрываем начало трубы, 105 - Парсим переменное окружение
 */
-void	ft_parent_process(int *pipe_fd, char **argv, char **env)
+void	parent_process(int *pipe_fd, char **argv, char **env)
 {
 	int		file2;
 	char	**path;
@@ -102,9 +102,9 @@ void	ft_parent_process(int *pipe_fd, char **argv, char **env)
 	dup2(pipe_fd[0], 0);
 	dup2(file2, 1);
 	close(pipe_fd[1]);
-	path = ft_pars_var_environ(env);
+	path = pars_var_environ(env);
 	command = ft_split(argv[3], ' ');
-	all_file_path = ft_collecting_path_cmd(path, command[0]);
+	all_file_path = collecting_path_cmd(path, command[0]);
 	if (!all_file_path)
 		perror("Command not found {ft_parent_process}");
 	execve(all_file_path, command, env);
@@ -126,8 +126,8 @@ int	main(int argc, char **argv, char **env)
 		return (0);
 	}
 	if (pid == 0)
-		ft_child_process(pipe_fd, argv, env);
+		child_process(pipe_fd, argv, env);
 	else
-		ft_parent_process(pipe_fd, argv, env);
+		parent_process(pipe_fd, argv, env);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,16 +6,12 @@
 /*   By: aleslie <aleslie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 18:06:58 by aleslie           #+#    #+#             */
-/*   Updated: 2021/11/11 21:50:45 by aleslie          ###   ########.fr       */
+/*   Updated: 2021/11/12 13:55:21 by aleslie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
-
-# define BED		0
-# define GOOD		1
-# define READ_END	2
 
 # include "libft/libft.h"
 # include <unistd.h>
@@ -23,10 +19,10 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-char	*ft_collecting_path_cmd(char **path, char *command);
-char	**ft_pars_var_environ(char **env);
-void	ft_child_process(int *pipe_fd, char **argv, char **env);
-void	ft_parent_process(int *pipe_fd, char **argv, char **env);
+char	*collecting_path_cmd(char **path, char *command);
+char	**pars_var_environ(char **env);
+void	child_process(int *pipe_fd, char **argv, char **env);
+void	parent_process(int *pipe_fd, char **argv, char **env);
 int		main(int argc, char **argv, char **env);
 
 #endif
